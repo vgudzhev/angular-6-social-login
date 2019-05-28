8ef706e89c4f6511bf094b609e3cd57ff227e1dc
Vasil <gudzhevv@vmware.com>
2019-05-29 00:17:14 +0300

    Add dist directory

---
+++ dist/entities/user.d.ts
@@ -0,0 +1,23 @@
+export declare class SocialUser {
+    provider: string;
+    id: string;
+    email: string;
+    name: string;
+    image: string;
+    token?: string;
+    idToken?: string;
+    expireIn?: string;
+    expireAt?: string;
+}
+export declare class LoginProviderClass {
+    name: string;
+    id: string;
+    url: string;
+}
+export declare class LinkedInResponse {
+    emailAddress: string;
+    firstName: string;
+    id: string;
+    lastName: string;
+    pictureUrl: string;
+}
